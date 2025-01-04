7fe13b5181674faa76af7be3a56eb8cb53225add
league <maddoxseul@sbsd.net>
2025-01-03 16:57:39 -0800

    nick will kiss shrek

--- _01_creating_objects/_c_turtle_race.py
+++ _01_creating_objects/_c_turtle_race.py
@@ -1,6 +1,7 @@
 """
 Turtle Race
 """
+from tkinter import messagebox, simpledialog
 import turtle
 import random
 from PIL import Image
@@ -33,67 +34,84 @@ if __name__ == '__main__':
     draw_background()
 
     # TODO 1) Create an empty list of turtles
+    turtles = []
 
-    william_beckwith = turtle.Turtle()
-    ur_dad_left_for_da_milk = turtle.Turtle()
-    sparsh_thakore = turtle.Turtle()
-    matteeshee = turtle.Turtle()
+
+
+
+
+    for i in range(8):
+        t = turtle.Turtle()
+        turtles.append(t)
+        t.shape('turtle')
+        t.speed(3)
+        t.penup()
+        t.shapesize(3)
+        t.goto(-415, 193 - i*55 )
 
     # TODO 2) Create a new turtle and set its shape to 'turtle
-    Pneumonoultramicroscopicsilicovolcanoconiosis_turtle = turtle.Turtle()
-    Pneumonoultramicroscopicsilicovolcanoconiosis_turtle.shape('turtle')
+    # Pneumonoultramicroscopicsilicovolcanoconiosis_turtle = turtle.Turtle()
+    # Pneumonoultramicroscopicsilicovolcanoconiosis_turtle.shape('turtle')
 
     # TODO 3) Set the turtle's speed to 3
-    Pneumonoultramicroscopicsilicovolcanoconiosis_turtle.speed(3)
+    # Pneumonoultramicroscopicsilicovolcanoconiosis_turtle.speed(3)
     # TODO 4) Set the turtle's pen up
-    Pneumonoultramicroscopicsilicovolcanoconiosis_turtle.penup()
+    # Pneumonoultramicroscopicsilicovolcanoconiosis_turtle.penup()
     # TODO 5) Use the turtle's goto() method to set its position on the left
     #  side of the screen
-    Pneumonoultramicroscopicsilicovolcanoconiosis_turtle.goto(-400, 200)
-    Pneumonoultramicroscopicsilicovolcanoconiosis_turtle.width(10)
+    # Pneumonoultramicroscopicsilicovolcanoconiosis_turtle.goto(-400, 200)
+    # Pneumonoultramicroscopicsilicovolcanoconiosis_turtle.width(10)
 
     # TODO 6) use a loop to repeat the previous instructions and create
     #  8 turtles lined up on the left side of the screen
     #  *HINT* click on the window to print the corresponding x, y location
-    william_beckwith.speed(10)
-    william_beckwith.penup()
-    william_beckwith.goto(-400, 140)
-    william_beckwith.shape('turtle')
+    # william_beckwith.speed(10)
+    # william_beckwith.penup()
+    # william_beckwith.goto(-400, 140)
+    # william_beckwith.shape('turtle')
 
-    ur_dad_left_for_da_milk.speed(3)
-    ur_dad_left_for_da_milk.penup()
-    ur_dad_left_for_da_milk.goto(-400, 78)
-    ur_dad_left_for_da_milk.shape('turtle')
+    # ur_dad_left_for_da_milk.speed(3)
+    # ur_dad_left_for_da_milk.penup()
+    # ur_dad_left_for_da_milk.goto(-400, 78)
+    # ur_dad_left_for_da_milk.shape('turtle')
 
-    sparsh_thakore.speed(10)
-    sparsh_thakore.penup()
-    sparsh_thakore.goto(-400, 27.5)
-    sparsh_thakore.shape('turtle')
+    # sparsh_thakore.speed(10)
+    # sparsh_thakore.penup()
+    # sparsh_thakore.goto(-400, 27.5)
+    # sparsh_thakore.shape('turtle')
 
-    matteeshee.speed(10)
-    matteeshee.penup()
-    matteeshee.goto(-400, -141)
-    matteeshee.shape('turtle')
-    matteeshee.shapesize(19)
+    # matteeshee.speed(10)
+    # matteeshee.penup()
+    # matteeshee.goto(-400, -141)
+    # matteeshee.shape('turtle')
+    # matteeshee.shapesize(19)
     # TODO 7) Move each turtle forward a random distance between 1 and 20
-    distance = random.randint(1, 20)
-    distance2 = random.randint(1, 20)
-    distance3 = random.randint(1, 20)
-    distance4 = random.randint(1, 20)
-    distance5 = random.randint(1, 20)
+    colors = ["black", "red", "blue", "green", "pink", "yellow", "orange", "cyan"]
+    racecomplete = False
+    winner = -1
+    while racecomplete == False:
+        for i in range(8):
+            t = turtles[i]
+            t.color(colors[random.randint(0,7)])
+            t.forward(random.randint(5,20))
+            if t.xcor() > 348:
+                racecomplete = True
+                messagebox.showinfo(title="shrek", message="turtle #" + str(i+1) + " won")
+                winner = i
+                break
 
-    Pneumonoultramicroscopicsilicovolcanoconiosis_turtle.forward(distance)
-    william_beckwith.forward(distance2)
-    ur_dad_left_for_da_milk.forward(distance3)
-    sparsh_thakore.forward(distance4)
-    matteeshee.forward(distance5)
+    while True:
+        turtles[winner].right(10)
+
     # TODO 8) Create a loop to keep moving each turtle until a turtle
     #  crosses the finish line
     #  *HINT* click on the window to print the corresponding x, y location
-    for i in range(50):
+
 
     # TODO 9) When a turtle crosses the finish line, stop the race and
     #  indicate which turtle won the race.
+
+
 
     # EXTRA: Create different colors for each turtle and code a special
     # dance for the winning turtle!
